a2a753a2e106cad61e6aff0b9ce5f6fa9f13b352
Dhruvisha Mondhe <63810182+Dhruvisha01@users.noreply.github.com>
2024-10-18 20:57:11 -0400

    Cleaned up code

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
-// server/server.js
 import express from 'express';
 import cors from 'cors'
 import OpenAI from 'openai';
-
 
 const app = express();
 
@@ -10,23 +8,18 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-
 const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY, })
 
-// Define a simple route
-app.get('/api', (req, res) => {
-    res.json({ message: 'Hello from the Node.js backend!' });
-});
-
+// API
 app.post('/api/chatbot', async (req, res) => {
     const { conversation } = req.body; // Conversation history passed from frontend
 
     try {
         const userMessage = conversation[conversation.length - 1].msg;
 
-        // Filter out any messages that are null or have an empty message content
+        // Filter out any messages that are null or have an empty message content to avoid errors
         const filteredMessages = conversation
-            .filter((chat) => chat.msg && chat.msg.trim()) // Filter out null/empty messages
+            .filter((chat) => chat.msg && chat.msg.trim())
             .map((chat) => ({
                 role: chat.sender === 'user' ? 'user' : 'assistant',
                 content: chat.msg,
@@ -34,10 +27,11 @@ app.post('/api/chatbot', async (req, res) => {
 
         // Call OpenAI API for a chat-based response
         const response = await openai.chat.completions.create({
-            model: "gpt-3.5-turbo", // Use the latest GPT model
+            model: "gpt-3.5-turbo", // Use the latest GPT model - Can also use GPT-4
+            // Explain the system's role and provide the conversation history
             messages: [
                 { role: 'system', content: 'You are DealBot, a chatbot for Groupon, designed to help users navigate the platform, answer general inquiries, and assist with finding deals. If users ask about deals or selling items on Groupon, prompt them to return to the main menu and select the appropriate option: "Find Deals" or "Sell on Groupon." Do not provide direct answers to questions about deals or selling, as these should always be handled through the menu options. \nKeep in mind: \n- Users are already on the Groupon platform, so avoid directing them to visit the website.\n- For FAQs, let users know you\'re redirecting them to the FAQ section and ask if they have a specific question they would like to type in. Your primary role is to handle other inquiries and ensure smooth navigation of the platform.' },
-                ...filteredMessages // Pass the filtered conversation history
+                ...filteredMessages 
             ],
             max_tokens: 150,
         });
